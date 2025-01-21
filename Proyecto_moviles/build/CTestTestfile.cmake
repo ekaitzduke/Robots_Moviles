21e9ccbd9ee74fde7c27b2bff31496bd386181d8
ekaitzduke <167782365+ekaitzduke@users.noreply.github.com>
2025-01-21 19:25:50 +0100

    Delete Proyecto_moviles directory

--- Proyecto_moviles/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/franciscocj/Proyecto_moviles/src
-# Build directory: /home/franciscocj/Proyecto_moviles/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("object_detection_mission")
--- Proyecto_moviles/build/object_detection_mission/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/franciscocj/Proyecto_moviles/src/object_detection_mission
-# Build directory: /home/franciscocj/Proyecto_moviles/build/object_detection_mission
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
